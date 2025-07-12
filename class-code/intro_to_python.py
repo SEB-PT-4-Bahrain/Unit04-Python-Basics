f7b7bc221bb780e2af63b315ebf5b4eb06061669
omarakamal <omaraakamal@gmail.com>
2025-07-12 11:11:38 +0300

    update

--- class-code/intro_to_python.py
+++ class-code/intro_to_python.py
@@ -2,10 +2,28 @@
 
 print("Like console.log")
 
+# declaring variables
 my_name = 'Omar'
 
 my_number = 6
 
 print(my_number)
+
+
+# handling constants in Python
+NEVER_CHANGING_VARIABLE = 500
+
+
+
+# type will tell us what data type our value is
+print(type(my_name))
+print(type(5.5))
+
+
+
+# Booleans
+
+print(type(True))
+print(False)
 
 
